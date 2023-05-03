bac648aad1e7ced55bfe28bbd77599232d43c6b3
Hvirda <virda.hiral@gmail.com>
2023-05-03 20:40:06 +0100

    change

--- Pattern/src/Pattern1.java
+++ Pattern/src/Pattern1.java
@@ -4,6 +4,7 @@ public class Pattern1 {
         for(int i=0;i<=4;i++){
             for(int j=0;j<=i;j++) {
                 System.out.print("***");
+                System.out.println("add line");
             }
             System.out.println();
         }
